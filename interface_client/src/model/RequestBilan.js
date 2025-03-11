96806d83f912760b7987134586c0afb7d0b8438f
Myohres <77498044+Myohres@users.noreply.github.com>
2025-03-12 00:48:41 +0100

    add gateway, connexion

---
+++ interface_client/src/model/RequestBilan.js
@@ -0,0 +1,9 @@
+export default class RequestBilan {
+    constructor(patId, patientNoteList, birthDay, gender) {
+        this.patId = patId;
+        this.patientNoteList = patientNoteList;
+        this.birthDay = birthDay;
+        this.gender = gender;
+
+    }
+}
