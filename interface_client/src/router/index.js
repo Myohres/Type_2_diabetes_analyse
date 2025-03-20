a54c79487462e6149bc010079aa19209b846ad74
Simon <bougard.simon@hotmail.fr>
2025-03-20 17:48:55 +0100

    change id with patid

--- interface_client/src/router/index.js
+++ interface_client/src/router/index.js
@@ -34,7 +34,7 @@ const router = createRouter({
       component: () => import('../views/Patient.vue'),
     },
     {
-      path: '/patient/:id',
+      path: '/patient/:patId',
       name: 'PatientPage',
       component: Patient,
       props : true,
