e5b85d7eaae2a72e88e43e7486c9fab80c89e789
Simon <bougard.simon@hotmail.fr>
2025-03-21 20:34:39 +0100

    getaway start

--- patient_bilan/src/main/java/medi_labo/patient_bilan/BilanController.java
+++ patient_bilan/src/main/java/medi_labo/patient_bilan/BilanController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/assessment")
+@RequestMapping("/bilan")
 @CrossOrigin(origins = "http://localhost:5173/")
 public class BilanController {
 
