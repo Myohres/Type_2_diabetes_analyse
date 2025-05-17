9fb277092cf195b29da6c8349b74213c5e3da272
Simon <bougard.simon@hotmail.fr>
2025-05-17 22:15:10 +0200

    test handlecontroller

--- patient-assessment/src/test/java/medi_labo/patient_assessment/controller/PatAssessmentControllerTest.java
+++ patient-assessment/src/test/java/medi_labo/patient_assessment/controller/PatAssessmentControllerTest.java
@@ -1,5 +1,6 @@
 package medi_labo.patient_assessment.controller;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import feign.FeignException;
 import medi_labo.patient_assessment.exception.CustomExceptions;
 import medi_labo.patient_assessment.integration.PatHistoryClient;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -59,29 +61,29 @@ class PatAssessmentControllerTest {
     void getPatAssessmentByPatId() throws Exception {
         when(patInformationClient.getBirthDayGenderByPatId(any())).thenReturn(ResponseEntity.ok(birthDayGenderDTO));
         when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
-        when(patAssessmentService.generatePatAssessment(any(),any(),any(),any())).thenReturn(new PatAssessment());
-        mockMvc.perform(post("/pat-assessment/patId/1")).andExpect(status().isOk());
+        when(patAssessmentService.generatePatAssessment(any(), any(), any(), any())).thenReturn(new PatAssessment());
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isOk());
     }
 
     @Test
     void getPatAssessmentByPatIdWhenPatInformationNotAvailable() throws Exception {
         when(patInformationClient.getBirthDayGenderByPatId(any())).thenReturn(ResponseEntity.badRequest().build());
         when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
-        mockMvc.perform(post("/pat-assessment/patId/1")).andExpect(status().isBadRequest());
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isBadRequest());
     }
 
     @Test
     void getPatAssessmentByPatIdWhenServiceNotAvailable() throws Exception {
         when(patInformationClient.getBirthDayGenderByPatId(any())).thenThrow(new CustomExceptions.ServiceUnavailableException("not found"));
         when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
-        mockMvc.perform(post("/pat-assessment/patId/1")).andExpect(status().isServiceUnavailable());
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isServiceUnavailable());
     }
 
     @Test
     void getPatAssessmentByPatIdWhenPatInformationNotFound() throws Exception {
         when(patInformationClient.getBirthDayGenderByPatId(any())).thenThrow(new CustomExceptions.ResourceNotFoundException("not found"));
         when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
-        mockMvc.perform(post("/pat-assessment/patId/1")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isNotFound());
     }
 
     @Test
@@ -89,16 +91,43 @@ class PatAssessmentControllerTest {
         birthDayGenderDTO = new BirthDayGenderDTO();
         birthDayGenderDTO.setGender("A");
         when(patInformationClient.getBirthDayGenderByPatId(any())).thenReturn(ResponseEntity.ok(birthDayGenderDTO));
-        mockMvc.perform(post("/pat-assessment/patId/1")).andExpect(status().isBadRequest());
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isBadRequest());
     }
-
-
 
     @Test
     void getPatAssessmentByPatIdWhenPatHistoryNotFound() throws Exception {
         when(patInformationClient.getBirthDayGenderByPatId(any())).thenReturn(ResponseEntity.ok(birthDayGenderDTO));
         when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenThrow(new CustomExceptions.ResourceNotFoundException("not found"));
-        mockMvc.perform(post("/pat-assessment/patId/1")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isNotFound());
     }
 
+    @Test
+    void getPatAssessmentByPatIdWhenLocaleDateNotValid() throws Exception {
+        when(patInformationClient.getBirthDayGenderByPatId(any())).thenThrow(HttpMessageNotReadableException.class);
+        when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getPatAssessmentByPatIdWhenBadRequest() throws Exception {
+        when(patInformationClient.getBirthDayGenderByPatId(any())).thenThrow(CustomExceptions.BadRequestException.class);
+        when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getPatAssessmentByPatIdWhenInternalError() throws Exception {
+        when(patInformationClient.getBirthDayGenderByPatId(any())).thenThrow(CustomExceptions.InternalServerErrorException.class);
+        when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void getPatAssessmentByPatIdWhenFeignException() throws Exception {
+        birthDayGenderDTO = new BirthDayGenderDTO();
+
+        when(patInformationClient.getBirthDayGenderByPatId(any())).thenThrow(FeignException.class);
+        when(patHistoryClient.getNoteListHistoriesByPatId(any())).thenReturn(ResponseEntity.ok(noteListHistories));
+        mockMvc.perform(get("/pat-assessment/patId/1")).andExpect(status().isBadGateway());
+    }
 }
